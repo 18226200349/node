c25a1e78979af622ebc10c7c4f235d5a4417ec9a
18226200349 <2023576033@qq.com>
2019-03-30 21:18:05 +0800

    提炼公共公共方法

---
+++ src/main/java/com/webi/util/ReverseNodeUtil.java
@@ -0,0 +1,60 @@
+package com.webi.util;
+
+import com.webi.nodeadmin.param.Node;
+
+/**
+ * Title: ReverseNodeUtil<br>
+ * Description: ReverseNodeUtil<br>
+ * Company:韦博英语在线教育部</br>
+ * CreateDate:2019年03月30日 21:14
+ *
+ * @author Administrator
+ */
+public class ReverseNodeUtil {
+    /**
+     * Title:  循环遍历反转链表<br>
+     * Description: 循环遍历反转链表<br>
+     * CreateDate: 2019/3/30 21:15<br>
+     *
+     * @category  循环遍历反转链表
+     * @author Administrator
+     * @param head
+     * @return com.webi.nodeadmin.param.Node
+     * @exception Exception
+     */
+    public static  Node reverse1(Node head) {
+
+        if(head==null){
+            return head;
+        }
+        //头结点
+        Node pre=head;
+        //下一节点
+        Node cur=head.getNext();
+        Node tmp;//临时节点
+        while(cur!=null){
+            //节点反转
+            tmp=cur.getNext();
+            cur.setNext(pre);
+            //指针移动
+            pre=cur;
+            cur=tmp;
+        }
+        head.setNext(null);
+        return  pre;
+    }
+    //递归反转链表
+    public static Node reverse(Node head) {
+        if(head==null||head.getNext()==null){
+            return  head;
+        }
+        // 先反转后续节点head.getNext()
+        Node reHead = reverse(head.getNext());
+        // 将当前结点的指针域指向前一结点
+        head.getNext().setNext(head);
+        // 前一结点的指针域令为null;
+        head.setNext(null);
+        // 反转后新链表的头结点
+        return reHead;
+    }
+}
--- src/test/java/com/webi/nodeadmin/NodeadminApplicationTests.java
+++ src/test/java/com/webi/nodeadmin/NodeadminApplicationTests.java
@@ -1,6 +1,7 @@
 package com.webi.nodeadmin;
 
 import com.webi.nodeadmin.param.Node;
+import com.webi.util.ReverseNodeUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,46 +30,19 @@ public class NodeadminApplicationTests {
 				h=h.getNext();
 			}
 			//递归反转
-//			Node headTmp=reverse(head);
-//		while (null != headTmp) {
-//			System.out.print(headTmp.getData() + " ");
-//			headTmp = headTmp.getNext();
-//		}
+			Node headTmp=ReverseNodeUtil.reverse(head);
+		while (null != headTmp) {
+			System.out.print(headTmp.getData() + " ");
+			headTmp = headTmp.getNext();
+		}
 		//循环反转
-		Node headTmp2=reverse1(head);
+		Node headTmp2=ReverseNodeUtil.reverse1(head);
 		while(null!=headTmp2){
 			System.out.print(headTmp2.getData()+" $ ");
 			headTmp2=headTmp2.getNext();
 		}
 	}
-    //循环遍历反转链表
-	private Node reverse1(Node head) {
-		if(head==null){
-			return head;
-		}
-		Node pre=head;//头结点
-		Node cur=head.getNext();//下一节点
-		Node tmp;//临时节点
-		while(cur!=null){
-			//节点反转
-            tmp=cur.getNext();
-            cur.setNext(pre);
-            //指针移动
-			pre=cur;
-			cur=tmp;
-		}
-		head.setNext(null);
-		return  pre;
-	}
-    //递归反转链表
-	private Node reverse(Node head) {
-		if(head==null||head.getNext()==null){
-			return  head;
-		}
-		Node reHead = reverse(head.getNext());// 先反转后续节点head.getNext()
-		head.getNext().setNext(head);// 将当前结点的指针域指向前一结点
-		head.setNext(null);// 前一结点的指针域令为null;
-		return reHead;// 反转后新链表的头结点
-	}
+
+
 
 }
