58a723e775e42c53330b9094c2734442c7d20a94
18226200349 <2023576033@qq.com>
2019-03-30 16:40:14 +0800

    初始化

---
+++ src/main/java/com/webi/nodeadmin/NodeadminApplication.java
@@ -0,0 +1,13 @@
+package com.webi.nodeadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodeadminApplication.class, args);
+	}
+
+}
